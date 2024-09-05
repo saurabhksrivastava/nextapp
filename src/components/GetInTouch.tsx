df29377db7726ac91e90e1cd65b93a86cc2b458e
Saurabh Kumar <techbnda@gmail.com>
2024-09-05 15:46:04 +0530

    Fix GetInTouch

--- src/components/GetInTouch.tsx
+++ src/components/GetInTouch.tsx
@@ -1,8 +1,8 @@
 "use client";
 import React from "react";
 import { cn } from "@/lib/utils";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
 
 export default function GetInTouch() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
